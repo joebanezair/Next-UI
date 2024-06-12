836cf5239475bf3cdae1032bd25dc0fbd88a92e5
Joebanezair <49142653+joebanezair@users.noreply.github.com>
2024-06-12 19:24:38 +0800

    Add files via upload
    command:
    - yarn
    - yarn dev
    - yarn build

---
+++ officia/app/layout.tsx
@@ -0,0 +1,68 @@
+import "@/styles/globals.css";
+import { Metadata, Viewport } from "next";
+import { Link } from "@nextui-org/link";
+import clsx from "clsx";
+
+import { Providers } from "./providers";
+
+import { siteConfig } from "@/config/site";
+import { fontSans } from "@/config/fonts";
+import { Navbar } from "@/components/navbar";
+import jsontext from '@/styles/globalstrings.json'
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html suppressHydrationWarning lang="en">
+      <head />
+      <body
+        className={clsx(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
+        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+          <div className="relative flex flex-col h-screen">
+            <Navbar />
+            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+              {children} 
+              {/* children ung page */}
+            </main>
+            <footer className="w-full flex items-center justify-center py-3">
+              <Link
+                isExternal
+                className="flex items-center gap-1 text-current"
+                href={jsontext.English.globalTextEnglish.LinkedinJoe}
+                title="nextui.org homepage"
+              >
+                <span className="text-default-600">Developer:</span>
+                <p className="text-primary">Joebanezair Buatona - The Great</p>
+              </Link>
+            </footer>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
+}
